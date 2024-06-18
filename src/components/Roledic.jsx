970e23c6d0fabcd1fa1e777ce6ca7021653ea2b5
tokesh23 <tokesh1998@gmail.com>
2024-06-18 16:06:11 +0530

    black btn

--- src/components/Roledic.jsx
+++ src/components/Roledic.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Roledic.css'
 
 const Roledic = () => {
   return (
-    <div>Roledic</div>
+    <div >
+      <div className='imgdiv'>
+      <img src="https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_1-512.png" alt=""  className='blackbox'/>
+      <p>Click on Dice  to roll</p>
+      </div>
+      </div>
   )
 }
 
